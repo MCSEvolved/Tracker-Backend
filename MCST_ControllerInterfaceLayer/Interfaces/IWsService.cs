a20aa35a75de74bc6b80c6339b870d35cf1eb145
Josian van Efferen <github@Josian.nl>
2023-07-16 13:21:49 +0200

    stash

--- MCST_ControllerInterfaceLayer/Interfaces/IWsService.cs
+++ MCST_ControllerInterfaceLayer/Interfaces/IWsService.cs
@@ -7,7 +7,6 @@ namespace MCST_ControllerInterfaceLayer.Interfaces
 	{
         Task SendCommandToComputer(int computerId, string command);
         Task NewComputerOverWS(Computer computer);
-        Task SendRequestInventoryCommand(int computerId);
         Task NewInventoryOverWS(Inventory inventory);
         Task NewLocationOverWS(Location location);
         Task NewMessageOverWS(Message message);
